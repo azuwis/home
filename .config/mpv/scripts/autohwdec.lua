bbb36dc0b6de33f80845be62ddd3ba26d55de408
Zhong Jianxin <azuwis@gmail.com>
2015-07-14 10:19:19 +0800

    mpv: Only set hwdec if hwdec == auto

--- .config/mpv/scripts/autohwdec.lua
+++ .config/mpv/scripts/autohwdec.lua
@@ -1,14 +1,17 @@
 function on_vo_change(name, value)
-    if value == "vdpau" then
-        hwdec = "vdpau"
-    elseif value == "vaapi" then
-        hwdec = "vaapi"
-    else
-        hwdec = "auto"
-    end
-    mp.set_property("hwdec", hwdec)
-    mp.msg.info("set hwdec to " .. hwdec)
     hwdec = mp.get_property("hwdec")
-    mp.msg.info("current hwdec is " .. hwdec)
+    if hwdec == "auto" then
+        if value == "vdpau" then
+            hwdec = "vdpau"
+        elseif value == "vaapi" then
+            hwdec = "vaapi"
+        end
+        if hwdec ~= "auto" then
+            mp.set_property("hwdec", hwdec)
+            mp.msg.info("set hwdec to " .. hwdec)
+            hwdec = mp.get_property("hwdec")
+            mp.msg.info("current hwdec is " .. hwdec)
+        end
+    end
 end
 mp.observe_property("current-vo", "string", on_vo_change)
