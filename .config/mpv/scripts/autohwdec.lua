c586b1aeca38a3f2ccb09e8c878e55c0fec12c4a
Zhong Jianxin <azuwis@gmail.com>
2015-11-20 22:10:57 +0800

    mpv: Fix autohwdec.lua

--- .config/mpv/scripts/autohwdec.lua
+++ .config/mpv/scripts/autohwdec.lua
@@ -1,12 +1,13 @@
 function on_vo_change(name, current_vo)
-    current_hwdec = mp.get_property("hwdec")
+    local current_hwdec = mp.get_property("hwdec")
+    local wanted_hwdec = nil
     if current_hwdec == "auto" then
         if current_vo == "vdpau" then
             wanted_hwdec = "vdpau"
         elseif current_vo == "vaapi" then
             wanted_hwdec = "vaapi"
         end
-        if wanted_hwdec ~= "auto" then
+        if wanted_hwdec ~= nil then
             mp.set_property("hwdec", wanted_hwdec)
             mp.msg.info("set hwdec to " .. wanted_hwdec)
             current_hwdec = mp.get_property("hwdec")
