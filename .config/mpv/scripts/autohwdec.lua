169f733652aad247e9b59b4e1ccbf050eb7d5dc8
Zhong Jianxin <azuwis@gmail.com>
2015-11-20 20:05:43 +0800

    mpv: autohwdec better variable names

--- .config/mpv/scripts/autohwdec.lua
+++ .config/mpv/scripts/autohwdec.lua
@@ -1,16 +1,16 @@
-function on_vo_change(name, vo)
-    hwdec = mp.get_property("hwdec")
-    if hwdec == "auto" then
-        if vo == "vdpau" then
-            hwdec = "vdpau"
-        elseif vo == "vaapi" then
-            hwdec = "vaapi"
+function on_vo_change(name, current_vo)
+    current_hwdec = mp.get_property("hwdec")
+    if current_hwdec == "auto" then
+        if current_vo == "vdpau" then
+            wanted_hwdec = "vdpau"
+        elseif current_vo == "vaapi" then
+            wanted_hwdec = "vaapi"
         end
-        if hwdec ~= "auto" then
-            mp.set_property("hwdec", hwdec)
-            mp.msg.info("set hwdec to " .. hwdec)
-            hwdec = mp.get_property("hwdec")
-            mp.msg.info("current hwdec is " .. hwdec)
+        if wanted_hwdec ~= "auto" then
+            mp.set_property("hwdec", wanted_hwdec)
+            mp.msg.info("set hwdec to " .. wanted_hwdec)
+            current_hwdec = mp.get_property("hwdec")
+            mp.msg.info("current hwdec is " .. current_hwdec)
         end
     end
 end
