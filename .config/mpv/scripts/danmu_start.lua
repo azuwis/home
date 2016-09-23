63d0c3a6c8ad0e10d6b5fe2b59c5535fdd8052a6
Zhong Jianxin <azuwis@gmail.com>
2016-09-23 16:45:36 +0800

    danmu: Add danmu_start.lua

---
+++ .config/mpv/scripts/danmu_start.lua
@@ -0,0 +1,23 @@
+local utils = require 'mp.utils'
+
+function ev_start_file()
+  local path = mp.get_property('path')
+  if path == nil then
+    return
+  end
+  local pat_vod = {
+    '^https?://www%.douyu%.com/(.+)$',
+    '^https?://www%.panda%.tv/(.+)$'
+  }
+  for k,v in pairs(pat_vod) do
+    streamer = string.match(path, v)
+    if streamer ~= nil then
+      local danmu = mp.find_config_file('scripts/danmu')
+      if danmu ~= nil then
+        utils.subprocess({args={danmu}})
+      end
+      break
+    end
+  end
+end
+mp.register_event('start-file', ev_start_file)
