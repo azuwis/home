640a620aff9a88482ee5470e373dec203d91b98a
Zhong Jianxin <azuwis@gmail.com>
2017-11-26 14:43:22 +0800

    surfingkeys: Init config

---
+++ .config/surfingkeys.js
@@ -0,0 +1,7 @@
+map('q', 'E');
+map('w', 'R');
+settings.focusFirstCandidate = true;
+settings.hintAlign = 'left';
+settings.interceptedErrors = ['*'];
+settings.omnibarSuggestionTimeout = 500;
+settings.tabsThreshold = 0;
