868cc81b68d94c0af751dd3d5de79c2f3ad46ee7
Zhong Jianxin <azuwis@gmail.com>
2017-11-27 14:05:45 +0800

    surfingkeys: Disable animation

--- .config/surfingkeys.js
+++ .config/surfingkeys.js
@@ -5,3 +5,8 @@ settings.hintAlign = 'left';
 settings.interceptedErrors = ['*'];
 settings.omnibarSuggestionTimeout = 500;
 settings.tabsThreshold = 0;
+settings.theme = `
+.expandRichHints, .collapseRichHints, .slideInBanner, .slideInRight, .slideOutRight {
+    animation-duration: 0s !important;
+}
+`;
