3dc7634842cb3fad07ed72f8428f7f4ef21aa8d9
Zhong Jianxin <azuwis@gmail.com>
2019-06-24 13:31:56 +0800

    ardour: Add midi velocity plugin

---
+++ .config/ardour5/scripts/midivel.lua
@@ -0,0 +1,50 @@
+ardour {
+  ["type"]    = "dsp",
+  name        = "MIDI Velcocity Curve",
+  category    = "Utility",
+  license     = "MIT",
+  author      = "Ardour Lua Task Force",
+  description = [[Midi Filter for Velocity Curve.]]
+}
+
+function dsp_ioconfig ()
+  return { { midi_in = 1, midi_out = 1, audio_in = 0, audio_out = 0}, }
+end
+
+function dsp_run (_, _, n_samples)
+  local cnt = 1;
+  function tx_midi (time, data)
+    midiout[cnt] = {}
+    midiout[cnt]["time"] = time;
+    midiout[cnt]["data"] = data;
+    cnt = cnt + 1;
+  end
+
+  local mapx = {0, 41, 63, 81, 102, 127}
+  local mapy = {0, 38, 61, 86, 117, 127}
+  function map_velocity (v)
+    local i = 1
+    for index, value in ipairs(mapx) do
+      if v > value then
+        i = index
+      else
+        break
+      end
+    end
+    return mapy[i] + math.floor((v - mapx[i]) * (mapy[i+1] - mapy[i]) / (mapx[i+1] - mapx[i]))
+  end
+
+  -- for each incoming midi event
+  for _,b in pairs (midiin) do
+    local t = b["time"] -- t = [ 1 .. n_samples ]
+    local d = b["data"] -- get midi-event bytes
+    local event_type
+    if #d == 0 then event_type = -1 else event_type = d[1] >> 4 end
+    if (#d == 3 and event_type == 9) then -- note on
+      d[3] = map_velocity (d[3])
+      tx_midi (t, d)
+    else -- pass thru all other events unmodified
+      tx_midi (t, d)
+    end
+  end
+end
