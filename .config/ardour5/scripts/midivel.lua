30013572acacd17e60f2542b4bc3fd0e4547db1b
Zhong Jianxin <azuwis@gmail.com>
2019-07-24 10:52:38 +0800

    midivel: Support velcocity curve presets

--- .config/ardour5/scripts/midivel.lua
+++ .config/ardour5/scripts/midivel.lua
@@ -1,7 +1,7 @@
 ardour {
   ["type"]    = "dsp",
   name        = "MIDI Velcocity Curve",
-  category    = "Utility",
+  category    = "Dynamics",
   license     = "MIT",
   author      = "Ardour Lua Task Force",
   description = [[Midi Filter for Velocity Curve.]]
@@ -11,7 +11,33 @@ function dsp_ioconfig ()
   return { { midi_in = 1, midi_out = 1, audio_in = 0, audio_out = 0}, }
 end
 
+function dsp_params ()
+  return
+    {
+      {
+        ["type"] = "input", name = "Velocity Curve", min = 1, max = 2, default = 1, enum = true, scalepoints =
+          {
+            ["Yamaha P-125 {10, 95, 105, 116, 127}, {0, 105, 116, 123, 127}"] = 1,
+            ["Yamaha P-125 {0, 41, 63, 81, 102, 127}, {0, 38, 61, 86, 117, 127}"] = 2,
+          }
+      }
+    }
+end
+
+local velocity_curves =
+  {
+    {
+      {10, 95, 105, 116, 127},
+      {0, 105, 116, 123, 127}
+    },
+    {
+      {0, 41, 63, 81, 102, 127},
+      {0, 38, 61, 86, 117, 127}
+    }
+  }
+
 function dsp_run (_, _, n_samples)
+
   local cnt = 1;
   function tx_midi (time, data)
     midiout[cnt] = {}
@@ -20,8 +46,10 @@ function dsp_run (_, _, n_samples)
     cnt = cnt + 1;
   end
 
-  local mapx = {0, 41, 63, 81, 102, 127}
-  local mapy = {0, 38, 61, 86, 117, 127}
+  local ctrl = CtrlPorts:array ()
+  local selected = ctrl[1]
+  local mapx = velocity_curves[selected][1]
+  local mapy = velocity_curves[selected][2]
   function map_velocity (v)
     local i = v // (127 // (#mapx - 1)) + 1
     while true do
@@ -33,7 +61,7 @@ function dsp_run (_, _, n_samples)
           return mapy[#mapy]
         end
         if v < mapx[i+1] then
-          return mapy[i] + math.floor((v - mapx[i]) * (mapy[i+1] - mapy[i]) / (mapx[i+1] - mapx[i]))
+          return mapy[i] + math.floor ((v - mapx[i]) * (mapy[i+1] - mapy[i]) / (mapx[i+1] - mapx[i]))
         else
           i = i + 1
         end
