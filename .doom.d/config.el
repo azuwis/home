240e72c225a05bb26060516dd0b51bcc31256007
Zhong Jianxin <azuwis@gmail.com>
2019-07-26 17:42:12 +0800

    doom-emacs: More config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,3 +3,5 @@
 ;; Place your private configuration here
 
 (setq doom-font (font-spec :family "Monospace" :size 22))
+(setq which-key-idle-delay 0.3)
+(setq projectile-project-search-path '("~/src/"))
