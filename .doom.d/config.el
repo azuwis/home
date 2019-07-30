e87ef409ad9fee6c743f896193d821220f4f938f
Zhong Jianxin <azuwis@gmail.com>
2019-07-30 10:08:10 +0800

    doom-emacs: Set theme to doom-gruvbox

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,5 +3,6 @@
 ;; Place your private configuration here
 
 (setq doom-font (font-spec :family "Monospace" :size 22))
-(setq which-key-idle-delay 0.3)
+(setq doom-theme 'doom-gruvbox)
 (setq projectile-project-search-path '("~/src/"))
+(setq which-key-idle-delay 0.3)
