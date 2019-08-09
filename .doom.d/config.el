3717fc88884a8fdfbc07fef407eec31474e971ea
Zhong Jianxin <azuwis@gmail.com>
2019-08-09 09:56:17 +0800

    doom-emacs: Enable and config web mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,3 +6,8 @@
 (setq doom-theme 'doom-gruvbox)
 (setq projectile-project-search-path '("~/src/"))
 (setq which-key-idle-delay 0.3)
+(add-to-list 'auto-mode-alist '("\\.svelte\\'" . web-mode))
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-script-padding 0)
