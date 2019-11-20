019458bc835b90093edfba0b51e61821f7abe22b
Zhong Jianxin <azuwis@gmail.com>
2019-11-20 19:41:23 +0800

    doom-emacs: Enable lua mode

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -118,7 +118,7 @@
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;ledger            ; an accounting system in Emacs
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
