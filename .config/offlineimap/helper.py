2b36850ebc6b3cfc301ab61a463b02a135b20fbc
Zhong Jianxin <azuwis@gmail.com>
2020-11-06 14:51:25 +0800

    offlineimap: Update helper

--- .config/offlineimap/helper.py
+++ .config/offlineimap/helper.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
 from subprocess import check_output
 
 
 def password(account):
     return check_output("pass show " + account, shell=True).splitlines()[0]
+
+if __name__ == '__main__':
+    import sys
+    print(password(sys.argv[1]))
